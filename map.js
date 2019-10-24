2fd18397fed72a7b6a6a7825a56d4dcce2fed88c
Yasir S <yasser9946.1@gmail.com>
2019-10-24 09:04:10 +0300

    aded the fils

---
+++ map.js
@@ -0,0 +1,63 @@
+// map map map !! 
+// 1 - conver it into an array of objects => [{name : "yasir" } , ..... ] 
+var array = ["yasser" , "ahmed" , "eber" , "atheer"]
+
+// var objects = // use map
+
+
+
+// console.log(objects)
+
+// =========================================================================================
+// 2-Make me a array where only ages of objects => [27 , 27.23 , ....]
+var array = [{name : "yasser" , age : 27} , {name : "ebere" , age : 27.23 },
+{name : "nahid" , age : 29 } , {name : "Ahmed" , age : 31 }  , {name : "Atheer" , age : 25 }  ]
+
+// var AgesOfages = //use map 
+
+
+
+
+// console.log(AgesOfages)
+
+// =========================================================================================
+// 3 - add id into this objects inside the array => [{name: "yasir" , age : 27  , id : 1} , ...] 
+var array = [{name : "yasser" , age : 27} ,  {name : "Ahmed" , age : 31 } ,{name : "ebere" , age : 27.23 },
+{name : "nahid" , age : 29 } ,  {name : "Atheer" , age : 25 }  ]
+
+// var arrayWithId = // use map 
+
+
+
+// console.log(arrayWithId)
+
+// =========================================================================================
+//  4 - import the array of student from the students.js file and : 
+// add to objects gifts  => [{name:'Wejdan'  , Gender :'female' , gift :"Flower" }, ..... ]
+// if Gender female the gift is Flower and if Gender male the gift is Coffee .
+
+// var studentsWithGifts = // use map 
+
+
+
+
+// console.log(studentsWithGifts)
+
+// =========================================================================================
+//                              !!+!!!+!  Bonus !+!!!+!! 
+// use only map! to conver the sentence to array of objects like =>[{name: "yasser" , age : 27  , id : 1} , ...]
+// and no undfinde plz !  
+var isThatArray = "Yasser 27 Ahmed 31 Ali 27 Ahlam 29 Atheer 25"
+
+
+
+
+
+// console.log("")
+
+// ok you solve it ! what about this one ? 
+// can you solve the bonus in one line ? 
+
+
+
+// console.log("")
---
+++ students.js
@@ -0,0 +1,8 @@
+const students = [{name:'Wejdan'  , Gender :'female' } , {name:'shahad'  , Gender :'female' },{name:'Doaa Alalawi'  , Gender :'female' },{name:'Ibtissam'  , Gender :'female' }
+,{name:'ashjan'  , Gender :'female' },{name:'abeer'  , Gender :'female' },{name:'Nujud'  , Gender :'female' },{name:'Diala'  , Gender :'female' }
+,{name:'Doaa AlHajjaji'  , Gender :'female' },{name:'sara'  , Gender :'female' },{name:'Raghad'  , Gender :'female' },{name:'Nouf'  , Gender :'female' }
+,{name:'Amal'  , Gender :'female' },{name:'Majd'  , Gender :'female' },{name:'Nada'  , Gender :'female' },
+{name:'Ahlam'  , Gender :'female' },{name:'Israa'  , Gender :'female' },{name:'Aisha'  , Gender :'female' }
+,{name:'mohammed'  , Gender :'male' },{name:'Yazeed'  , Gender :'male' },{name:'Tareq'  , Gender :'male' },{name:'Fahad'  , Gender :'male' },{name:'Abdullah'  , Gender :'male' }
+,{name:'Hamed'  , Gender :'male' },{name:'Suhaib'  , Gender :'male' },{name:'Hisham'  , Gender :'male' },{name:'Abdulmajed'  , Gender :'male' },{name:'Ali'  , Gender :'male' },
+{name:'Midhat'  , Gender :'male' },{name:'mohammed Salah'  , Gender :'male' },{name:'Anas'  , Gender :'male' },{name:'Abdulgadir'  , Gender :'male' } ]
