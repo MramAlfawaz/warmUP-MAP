1683d4a96a2a3737ce4e0c0d46c1bcc51842f3e8
unknown <yasser9946.1@gmail.com>
2020-04-06 23:12:44 +0300

    update

--- students.js
+++ students.js
@@ -1,8 +1,29 @@
-const students = [{name:'Wejdan'  , Gender :'female' } , {name:'shahad'  , Gender :'female' },{name:'Doaa Alalawi'  , Gender :'female' },{name:'Ibtissam'  , Gender :'female' }
-,{name:'ashjan'  , Gender :'female' },{name:'abeer'  , Gender :'female' },{name:'Nujud'  , Gender :'female' },{name:'Diala'  , Gender :'female' }
-,{name:'Doaa AlHajjaji'  , Gender :'female' },{name:'sara'  , Gender :'female' },{name:'Raghad'  , Gender :'female' },{name:'Nouf'  , Gender :'female' }
-,{name:'Amal'  , Gender :'female' },{name:'Majd'  , Gender :'female' },{name:'Nada'  , Gender :'female' },
-{name:'Ahlam'  , Gender :'female' },{name:'Israa'  , Gender :'female' },{name:'Aisha'  , Gender :'female' }
-,{name:'mohammed'  , Gender :'male' },{name:'Yazeed'  , Gender :'male' },{name:'Tareq'  , Gender :'male' },{name:'Fahad'  , Gender :'male' },{name:'Abdullah'  , Gender :'male' }
-,{name:'Hamed'  , Gender :'male' },{name:'Suhaib'  , Gender :'male' },{name:'Hisham'  , Gender :'male' },{name:'Abdulmajed'  , Gender :'male' },{name:'Ali'  , Gender :'male' },
-{name:'Midhat'  , Gender :'male' },{name:'mohammed Salah'  , Gender :'male' },{name:'Anas'  , Gender :'male' },{name:'Abdulgadir'  , Gender :'male' } ]
+const students = [{name:'Asma'  , Gender :'female' } , {name:'Bashayr'  , Gender :'female' },{name:'Ghadeer Sadruddien'  , Gender :'female' },{name:'Ghadeer'  , Gender :'female' }
+,{name:'Ibtihal'  , Gender :'female' },{name:'Jawaher'  , Gender :'female' }
+,{name:'Halah'  , Gender :'female' },{name:'Hanan'  , Gender :'female' },{name:'Maram Ghilani'  , Gender :'female' },{name:'Maram al Fawaz'  , Gender :'female' }
+,{name:'Lujen'  , Gender :'female' },{name:'Malak'  , Gender :'female' },{name:'Najlaa'  , Gender :'female' },
+{name:'Ola'  , Gender :'female' },{name:'Rawan'  , Gender :'female' },{name:'Samiah'  , Gender :'female' },{name:'Sarah'  , Gender :'female' }
+,{name:'Jaber'  , Gender :'male' },{name:'mohammed Alhaddad'  , Gender :'male' },{name:'Abdulrahman Saddeek'  , Gender :'male' },{name:'Ziad'  , Gender :'male' },{name:'Mohammad Aljagthmi'  , Gender :'male' }
+,{name:'Ibrahim'  , Gender :'male' },{name:'Hamzah'  , Gender :'male' },{name:'Faisal'  , Gender :'male' },{name:'Edrees'  , Gender :'male' },{name:'Adel'  , Gender :'male' },
+{name:'Abdulrahman Albudayri'  , Gender :'male' },{name:'Abdulhameed'  , Gender :'male' },{name:'Abdulaziz Althagafi'  , Gender :'male' },{name:'Abdulaziz Alsubhi'  , Gender :'male' } ]
+
+
+// Asma 
+// Bashayr
+// Ghadeer 
+// Ghadeer 
+// Halah 
+// Hanan
+// Ibtihal 
+// Jawaher 
+// Lujen 
+// Malak 
+// Maram 
+// Maram 
+// Najlaa 
+// Ola
+// Rawan 
+// Samiah 
+// Sarah 
+
+console.log(students.length)
